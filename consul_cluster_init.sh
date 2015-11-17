52fbb04f0ec3eb3672ee08648e20b7fdfd07624d
Goacid <goacid@kurty.net>
2015-11-17 10:23:21 +0100

    Create consul_cluster_init.sh

---
+++ consul_cluster_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for entry in $(etcdctl ls /registrator --recursive 2>/dev/null) ;
+do
+  b=$(grep -Eo "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}" <<< ${entry})$(etcdctl get ${entry} 2>/dev/null|grep 53);
+  a+=" "$b;
+done;
+c=$(echo $a|tr " " "\\n"|sort|uniq|sed -re "s/(^10.[0-9.]*):.*/-retry-join=\\1/");
+if [[ $c ]]; then
+  total_server="$(echo ${c} | tr " " "\n"| grep -c join )"
+  if [[  total_server < 3 ]]; then
+    echo "-server $c"
+  else
+    echo "$c"
+  fi
+else
+  echo "-server -bootstrap-expect 3"
+fi
+
