c2b39b19d51f8552b77ef195a38963922c13a2ba
Goacid <goacid@kurty.net>
2015-12-11 13:52:30 +0100

    Update consul_cluster_init.sh

--- consul_cluster_init.sh
+++ consul_cluster_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for entry in $(etcdctl ls /registrator --recursive 2>/dev/null) ;
 do
-  b=$(grep -Eo "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}" <<< ${entry})$(etcdctl get ${entry} 2>/dev/null|grep 53);
+  b=$(grep -Eo "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}" <<< ${entry})$(etcdctl get ${entry} 2>/dev/null|grep "10.*:53");
   a+=" "$b;
 done;
 c=$(echo $a|tr " " "\\n"|sort|uniq|sed -re "s/(^10.[0-9.]*):.*/-retry-join=\\1/");
